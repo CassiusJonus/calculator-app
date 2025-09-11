9cd4643db242417bbf9aa350a9d6c07cdd3f11fa
Cassius Jonus <vegetafan5@gmail.com>
2025-09-11 08:43:11 -0500

    Removed ZIO dependency from the project

--- 01-core/src/main/scala/io/github/cassius/jonus/calculator/expression/Dependency.scala
+++ 01-core/src/main/scala/io/github/cassius/jonus/calculator/expression/Dependency.scala
@@ -1,10 +1,8 @@
 package io.github.cassius.jonus.calculator
 package expression
 
-import zio.*
-
 /*
 Contains definitions for the main functionality of the application. This trait
  */
 trait Dependency[A]:
-  def evaluate(expr: MathExpr[A]): UIO[A]
+  def evaluate(expr: MathExpr[A]): A
