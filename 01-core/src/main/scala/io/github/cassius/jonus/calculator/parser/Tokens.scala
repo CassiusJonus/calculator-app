055266ba05f579a809d90f4df9456be571e739d1
Cassius Jonus <vegetafan5@gmail.com>
2025-09-11 15:41:54 -0500

    Created file to store parser tokens

---
+++ 01-core/src/main/scala/io/github/cassius/jonus/calculator/parser/Tokens.scala
@@ -0,0 +1,27 @@
+package io.github.cassius.jonus.calculator
+package parser
+
+import cats.parse.Rfc5234
+import cats.parse.*
+
+/*
+Contains tokens that will be used to parse math expressions and terminal
+commands
+ */
+object Tokens:
+  val plus =
+    Parser.char('+')
+  val minus =
+    Parser.char('-')
+  val times =
+    Parser.char('*')
+  val dividedBy =
+    Parser.char('/')
+  val exp =
+    Parser.char('^')
+  val help =
+    Parser.string("help")
+  val exit =
+    Parser.string("exit")
+  val eval =
+    Parser.string("eval")
